516225371ec963047c1edf9a1916aaf079e0686c
felipscarvalho <felipecarvalho5520@gmail.com>
2025-08-31 23:07:02 -0300

    fix: mudança na proporção da aplicação java

--- java/src/main/java/com/poo/java/App.java
+++ java/src/main/java/com/poo/java/App.java
@@ -1,17 +1,6 @@
 package com.poo.java;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import java.util.List;
-
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -22,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, 1200, 800);
         stage.setScene(scene);
         stage.setTitle("Sisu BI");
         stage.show();
